33682d03e578ac83dcada11df69349c47d2c24c3
dfvasia <vasilii-zhuravsk@mail.ru>
2022-04-18 22:54:26 +0300

    8

--- companies/models.py
+++ companies/models.py
@@ -4,3 +4,10 @@ from django.db import models
 class Company(models.Model):
     name = models.CharField(max_length=20)
     logo = models.ImageField(upload_to='logos/')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Компания"
+        verbose_name_plural = "Компании"
