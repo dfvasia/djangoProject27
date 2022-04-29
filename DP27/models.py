a830d368c40cce392d7d6c97229abebabc6c9b73
Василий <vasilii-zhuravsk@mail.ru>
2022-04-29 15:40:16 +0300

    292

--- DP27/models.py
+++ DP27/models.py
@@ -34,6 +34,7 @@ class Vacancy(models.Model):
     class Meta:
         verbose_name = "Вакансия"
         verbose_name_plural = "Вакансии"
+        ordering = ['id']
 
     def __str__(self):
         return self.slug
--- DjangoProject27/settings.py
+++ DjangoProject27/settings.py
@@ -160,5 +160,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 10,
+    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    "PAGE_SIZE": 2,
 }
